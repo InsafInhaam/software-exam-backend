64d37181c2c12a644e0d0377739dfc5dd9b52b3f
Insaf Inhaam <insafinhaam732@gmail.com>
2023-03-07 17:45:43 +0530

    make it better5

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("software_db", "root", "", {
-  host: "localhost",
+const db = new Sequelize("freedb_software_db", "freedb_insafinhaam", "G5bET$nr9kqGbG9", {
+  host: "sql.freedb.tech",
   dialect: "mysql",
 })
 
